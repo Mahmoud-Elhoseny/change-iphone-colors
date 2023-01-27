11ad1e5243c7931ba8747794874ddbed42f79e9c
Mahmoud-Elhoseny <elhoseny916@gmail.com>
2023-01-27 05:43:33 +0300

    .

---
+++ main.js
@@ -0,0 +1,15 @@
+let container = document.querySelector(".container")
+let currentImg = document.querySelector(".current_img")
+console.log(currentImg);
+
+
+
+
+function changePhone(phone) {
+    currentImg.src = phone
+    
+}
+
+function ChangeColors(color) {
+    container.style.background = color
+}
